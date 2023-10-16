d8fdd3855934259e0f78e6d9d83685715ecacab9
root <esteban.guillen.pro@gmail.com>
2023-10-16 07:46:48 -0700

    i'm running out of jokes

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Digits
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char chara;
+for (chara = 'a'; chara <= 'z'; chara++)
+{
+putchar(chara);
+}
+putchar('\n');
+return (0);
+}
