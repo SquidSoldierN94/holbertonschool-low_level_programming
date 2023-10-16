0aea415d3aaddf9e4cc3809027f2068d3fe9cd7b
root <esteban.guillen.pro@gmail.com>
2023-10-16 04:56:59 -0700

    is the machine happy ??

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
-/* tells you what's up
-/*
-/* betty style doc for function main goes there */
+* tells you what's up
+*
+* betty style doc for function main goes there
+*/
 int main(void)
 {
 	int n;
