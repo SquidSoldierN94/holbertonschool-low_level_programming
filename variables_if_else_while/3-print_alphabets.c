a5020328443f295cea368d6f2310925a518a9fe9
root <esteban.guillen.pro@gmail.com>
2023-10-16 08:01:57 -0700

    betty have mercy on our souls

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 char chara;
- for (chara = 'a' ; chara <= 'z' ; chara++)
+for (chara = 'a' ; chara <= 'z' ; chara++)
 {
 putchar(chara);
 }
-for (chara = 'A' ; chara <= 'Z' ; chara++)
+for (chara = 'A'; chara <= 'Z'; chara++)
 {
 putchar(chara);
 }
