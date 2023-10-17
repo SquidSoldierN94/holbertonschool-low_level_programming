ab257bf314d053ac9fadf52327104f8633280e63
root <esteban.guillen.pro@gmail.com>
2023-10-17 01:37:28 -0700

    do i dread this potencial meeting, or am i yearning for it

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Digits
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char chara;
+for (chara = 'z'; chara >= 'a'; chara++)
+{
+putchar(chara);
+}
+putchar('\n');
+return (0);
+}
