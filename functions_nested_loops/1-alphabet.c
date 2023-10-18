73f51496c4b968d76b95ed6cc059ef26fd5e96c2
root <esteban.guillen.pro@gmail.com>
2023-10-18 06:18:04 -0700

    i thought we came to an understanding

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet - Digits
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char chara;
+for (chara = 'a'; chara <= 'z'; chara++)
+{
+_putchar(chara);
+}
+_putchar('\n');
+return (0);
+}
