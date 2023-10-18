42ef5d27bea87fb10f2d02827a7a1b502d13c2ed
root <esteban.guillen.pro@gmail.com>
2023-10-18 06:38:53 -0700

    i hate that he was right, but at least he helped

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Digits
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void);
+{
+char chara;
+ while(a < 10){
+for (chara = 'a'; chara <= 'z'; chara++)
+{
+putchar(chara);
+}
+}
+putchar('\n');
+}
