c0b3d3b23e099f4f0b31685e0d591c521f5d909e
root <esteban.guillen.pro@gmail.com>
2023-10-18 07:20:24 -0700

    guess there's another workout comming

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,15 @@
 *
 *Return: Always 0 (Success)
 */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-char chara;
- while(a < 10){
-for (chara = 'a'; chara <= 'z'; chara++)
-{
-putchar(chara);
-}
-}
-putchar('\n');
+ char chara;
+ while(chara < 10)
+ {
+  for (chara = 'a'; chara <= 'z'; chara++)
+   {
+    _putchar(chara);
+   }
+  }
+ _putchar('\n');
 }
