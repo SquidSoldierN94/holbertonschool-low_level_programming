00c1433977e7467a383c4ac29aa60943028a8e9b
root <esteban.guillen.pro@gmail.com>
2023-10-19 07:58:48 -0700

    fuck dem non dot c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower - Checks for lowercase character
+*@c: The character to be checked
+*
+*Return: 1 for lowercase character or 0 for anything else
+*/
+int _isalpha(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
