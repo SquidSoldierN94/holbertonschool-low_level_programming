5c64ec96d1264d71aa1570768e49a772f81191b8
root <esteban.guillen.pro@gmail.com>
2023-10-20 05:47:02 -0700

    how about that for a trade

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_islower - Checks for lowercase character
+*@c: The character to be checked
+*
+*Return: 1 for lowercase character or 0 for anything else
+*/
+int _abs(int c)
+{
+int abs_val;
+if (c < 0)
+{
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
