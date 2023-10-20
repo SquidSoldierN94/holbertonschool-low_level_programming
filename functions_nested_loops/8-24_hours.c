48befe0796330f71c75683b326f8da351c6f7748
root <esteban.guillen.pro@gmail.com>
2023-10-20 07:09:33 -0700

    BECAUSE I CAN FIX THAT

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*print_last_digit - Prints the last digit of a number
+*: The number to be treated
+*
+*Return: Value of the last digit of number
+*/
+void jack_bauer(void)
+{
+int m2, m1, s2, s1;
+{
+for (s1 = 0; s1 < 9; s1++)
+{
+for (s2 = 0; s2 < 9; s2++)
+{
+for (m1 = 0; m1 < 3; m1++)
+{
+for (m2 = 0; m2 < 2; m2++)
+{
+if (m2 < 2 && m1 < 3)
+{
+_putchar(m2 + '0');
+_putchar(m1 + '0');
+_putchar(58);
+_putchar(s2 + '0');
+_putchar(s1 + '0');
+}
+}
+}
+}
+}
+}
+}
