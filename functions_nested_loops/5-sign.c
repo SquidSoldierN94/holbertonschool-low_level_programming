70915ec690604a17228b0f4eccfce3b1aaa6f22d
root <esteban.guillen.pro@gmail.com>
2023-10-20 01:44:50 -0700

    gaming

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
 *print_sign - Numbers
-*@c: The character to be checked
+*@n: The character to be checked
 *
 *Return: 1 for lowercase character or 0 for anything else
 */
 int print_sign(int n)
 {
-if (n >= 0)
+if (n > 0)
 {
 _putchar('+');
- return (1);
+return (1);
 }
 if (n == 0)
 {
 _putchar('0');
 return (0);
 }
-if (n <= 0)
+if (n < 0)
 {
 _putchar('-');
 return (-1);
