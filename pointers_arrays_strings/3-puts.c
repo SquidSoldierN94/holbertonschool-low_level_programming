28d2da4539b1542e6404306ba34dcb8b0e562ebd
root <esteban.guillen.pro@gmail.com>
2023-10-24 00:41:21 -0700

    that's some weird ass music tbh

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -7,9 +7,12 @@
 */
 void _puts(char *str)
 {
-char chara = "let's hope this works";
-for (chara = 0; chara < 1; chara++)
+for (; *str != '\0';)
 {
-_puts(chara);
- _putchar("\n");
+_putchar (*str);
+}
+{
+_putchar ("\n");
+}
+}
 }
