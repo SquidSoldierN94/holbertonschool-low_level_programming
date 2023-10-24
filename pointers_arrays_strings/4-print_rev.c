abb71603a6b5a4ff85931107793f5ca877a4aeb4
root <esteban.guillen.pro@gmail.com>
2023-10-24 00:57:18 -0700

    this one seems easy

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_rev - Funky Donkey 
+*@s - its a thing
+*
+*Return: void
+*/
+void print_rev(char *s);
+{
+s = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+for(; s != '\0'; s++)
+print_rev(s);
+}
