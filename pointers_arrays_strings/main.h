014fc57bf49a7f6ebf030ed00be2361440c69cef
root <esteban.guillen.pro@gmail.com>
2023-10-24 06:39:12 -0700

    maybe imma start using dem commits to express myself

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef main_h
 #define main_h
 
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
 void print_rev(char *s);
