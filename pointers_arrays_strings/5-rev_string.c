4d5ad9b171a0718e538bbc5ffe49e19f70deeb17
root <esteban.guillen.pro@gmail.com>
2023-10-24 02:04:08 -0700

    it cant be THAT easy, right

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_puts - Does shit idk
+*
+*@s: The string to print
+*Return: void
+*/
+void rev_string(char *s);
+{
+for (; *s != '\0'; s++)
+{
+_putchar (s);
+}
+{
+_putchar ('\n');
+}
+}
