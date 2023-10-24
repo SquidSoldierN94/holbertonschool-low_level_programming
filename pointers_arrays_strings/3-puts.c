040fc2abd23e905e49d13d3097943c90887d98b0
root <esteban.guillen.pro@gmail.com>
2023-10-24 00:31:01 -0700

    think this new system might work

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*puts - Does shit idk
+*
+*
+*/
+void _puts(char *str)
+{
+char chara = "let's hope this works";
+for (chara = 0; chara < 1; chara++)
+{
+_puts(chara);
+ _putchar("\n");
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 char *_strcat(char *dest, char *src);
