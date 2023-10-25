7858e0ef3d75ad93dd46d2a365d1585eeabc0c17
root <esteban.guillen.pro@gmail.com>
2023-10-25 00:56:57 -0700

    could i be getting the hang of things ?

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strcpy - Copies a string to a buffer i think
+*@src - the source of the string
+*@dest - where the string is copied
+*
+*/
+char *_strcpy(char *dest, char *src);
+{
+memset (dest, 0, sizeof dest);
+strncpy (dest, src, sizeof dest -1);
+}
+{
+return (0);
+}
