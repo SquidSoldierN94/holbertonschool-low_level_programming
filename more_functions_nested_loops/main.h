8fb2ca0bac63101f406887f8db3634c13137a4a0
root <esteban.guillen.pro@gmail.com>
2023-10-25 09:37:28 -0700

    BURN IN HOLY FIRE

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,18 @@
 #ifndef main_h
 #define main_h
 
+int add(int, int);
+void print_to_98(int n);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+int _putchar(char c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
