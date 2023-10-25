20df85d08f39aab4de5a078eab209426984dd716
root <esteban.guillen.pro@gmail.com>
2023-10-25 01:04:14 -0700

    still forgetting about these damned semi colons

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,12 @@
 *
 */
 char *_strcpy(char *dest, char *src)
+{
 int deez;
 for (deez = 0; deez < 1; deez++)
 {
 memset (dest, 0, sizeof dest);
 strncpy (dest, src, sizeof dest -1);
 }
-{
 return (0);
 }
