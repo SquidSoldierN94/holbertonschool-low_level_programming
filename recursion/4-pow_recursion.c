b4c41b038d5f6c82133d8899666dc440e054990d
root <esteban.guillen.pro@gmail.com>
2023-10-30 06:10:16 -0700

    same as the last one. just use the fucking command lmao

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_pow_recursion - This persecution or somthin
+*
+*
+*Return: Nothing
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 1)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
