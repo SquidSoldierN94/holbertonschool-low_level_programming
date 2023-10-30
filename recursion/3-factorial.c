394e0f0ff3e06773d20c5e9b35b11d4e8fdf2f2d
root <esteban.guillen.pro@gmail.com>
2023-10-30 05:53:59 -0700

    truth be told, i just wanna be acknoledged

---
+++ recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_factorial - Deez nuts
+*
+*Return: Nothing
+*/
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n <= 1)
+return (1);
+return (n * factorial(n - 1));
+}
