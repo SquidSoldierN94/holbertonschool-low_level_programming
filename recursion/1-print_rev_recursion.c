1df73e757ae0ca7e28ffd62cb0927f36fcab4ad3
root <esteban.guillen.pro@gmail.com>
2023-11-03 03:21:04 -0700

    i hope they good tbh

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_print_rev_recursion - Does shit idk
+*
+*@s: One of the things
+*
+*Return: Nothing
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
