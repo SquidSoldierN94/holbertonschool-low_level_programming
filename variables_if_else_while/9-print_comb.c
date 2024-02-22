eb6f9f23c1e71c461f3162d34dabd570d39e21f7
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-02-22 11:28:37 +0100

    wyrm

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,22 +10,14 @@
  */
 int main(void)
 {
-    int range_start1 = 0;
-    int range_end1 = 9;
+int maggot=0 
 
-    int range_start2 = 0;
-    int range_end2 = 9;
-
-    int variable1, variable2;
-
-    for (variable1 = range_start1; variable1 <= range_end1; variable1++)
+    for (;maggot < 10; maggot++)
     {
-        for (variable2 = range_start2; variable2 <= range_end2; variable2++)
         {
-            putchar(variable1 + '0');
+            putchar(maggot)
             putchar(',');
             putchar(' ');
-            putchar(variable2 + '0');
         }
     }
 
