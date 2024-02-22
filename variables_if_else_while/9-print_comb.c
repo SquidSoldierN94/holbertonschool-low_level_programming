5991676cfdbabe962712f5238ab438ef1f93d6f0
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-02-22 11:25:08 +0100

    wyrm

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,6 +3,9 @@
 /**
  * main - Entry point of the program
  *
+ * This function prints pairs of numbers separated by a comma and a space.
+ * The first number ranges from 0 to 9, and the second number ranges from 0 to 9.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,9 +16,11 @@ int main(void)
     int range_start2 = 0;
     int range_end2 = 9;
 
-    for (int variable1 = range_start1; variable1 <= range_end1; variable1++)
+    int variable1, variable2;
+
+    for (variable1 = range_start1; variable1 <= range_end1; variable1++)
     {
-        for (int variable2 = range_start2; variable2 <= range_end2; variable2++)
+        for (variable2 = range_start2; variable2 <= range_end2; variable2++)
         {
             putchar(variable1 + '0');
             putchar(',');
