a8df71a1c75a6dc4bb66d7191447bf70bc84fd05
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-02-22 11:05:12 +0100

    should work

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,13 +8,12 @@
 int main(void)
 {
     int ascii_value;
-    ascii_value = 97;
+    ascii_value = 122;
 
-    while (ascii_value != 122)
-    {
-    for (ascii_value = 97; ascii_value <= 122; ascii_value--)
+    while (ascii_value >= 97)
     {
         putchar(ascii_value);
+        ascii_value--
     }
     }
     putchar(10);
