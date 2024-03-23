d868b06ee0fb5f6f1e9eedfb0631e13097d40889
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-03-23 03:37:39 +0100

    borb

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,22 +1,19 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int _putchar(char c);
-
 int main(int argc, char *argv[])
 {
     int num1, num2, result;
     int (*op_func)(int, int);
-    int temp;
-    int digits;
-    char *buffer;
-    const char *ptr;
 
     if (argc != 4)
     {
-        const char *error = "Error\n";
+        char *error = "Error\n";
         while (*error)
-            _putchar(*error++);
+        {
+            _putchar(*error);
+            error++;
+        }
         return (98);
     }
 
@@ -24,56 +21,32 @@ int main(int argc, char *argv[])
     num2 = atoi(argv[3]);
     op_func = get_op_func(argv[2]);
 
-    if (!op_func)
+    if (op_func == NULL)
     {
-        const char *error = "Error\n";
+        char *error = "Error\n";
         while (*error)
-            _putchar(*error++);
+        {
+            _putchar(*error);
+            error++;
+        }
         return (99);
     }
 
     if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
     {
-        const char *error = "Error\n";
+        char *error = "Error\n";
         while (*error)
-            _putchar(*error++);
+        {
+            _putchar(*error);
+            error++;
+        }
         return (100);
     }
 
     result = op_func(num1, num2);
 
-    if (result < 0)
-    {
-        _putchar('-');
-        result *= -1;
-    }
-
-    temp = result;
-    digits = 0;
-    while (temp > 0)
-    {
-        temp /= 10;
-        digits++;
-    }
-
-    buffer = malloc(digits + 1);
-    if (buffer == NULL)
-        return (1);
-
-    buffer[digits] = '\0';
-    while (digits--)
-    {
-        buffer[digits] = result % 10 + '0';
-        result /= 10;
-    }
-
-    ptr = buffer;
-    while (*ptr)
-        _putchar(*ptr++);
-
+    _putchar(result + '0');
     _putchar('\n');
-
-    free(buffer);
 
     return (0);
 }
