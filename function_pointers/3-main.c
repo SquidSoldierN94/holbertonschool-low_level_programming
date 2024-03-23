c22a994c97553bfe28584718a0b14b5d4bc432e4
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-03-23 03:49:16 +0100

    curse of ra

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,34 +1,43 @@
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int _putchar(char c);
-
-int main(int argc, char *argv[])
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+	int num1, num2;
+	char *op;
 
-    if (argc != 4 || !argv[2][1])
-        return (_putchar('E') + _putchar('r') + _putchar('r') + _putchar('o') + _putchar('r') + _putchar('\n') + 98);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-    if (!op_func || ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0))
-        return (_putchar('E') + _putchar('r') + _putchar('r') + _putchar('o') + _putchar('r') + _putchar('\n') + 99);
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    result = op_func(num1, num2);
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    if (result < 0)
-    {
-        _putchar('-');
-        result *= -1;
-    }
+	printf("%d\n", get_op_func(op)(num1, num2));
 
-    _putchar(result / 10 + '0');
-    _putchar(result % 10 + '0');
-    _putchar('\n');
-
-    return (0);
+	return (0);
 }
