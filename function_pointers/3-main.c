a125852d7a94f522572e0a13ffb806d3bc5fea47
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-03-23 03:41:51 +0100

    borb

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,41 +8,22 @@ int main(int argc, char *argv[])
     int num1, num2, result;
     int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        char *error = "Error\n";
-        while (*error)
-        {
-            _putchar(*error);
-            error++;
-        }
-        return (98);
-    }
+    if (argc != 4 || !argv[2][1])
+        return (_putchar('E') + _putchar('r') + _putchar('r') + _putchar('o') + _putchar('r') + _putchar('\n') + 98);
 
     num1 = atoi(argv[1]);
     num2 = atoi(argv[3]);
     op_func = get_op_func(argv[2]);
 
-    if (!op_func || (*argv[2] == '/' && num2 == 0))
-    {
-        char *error = "Error\n";
-        while (*error)
-        {
-            _putchar(*error);
-            error++;
-        }
-        return (!op_func ? 99 : 100);
-    }
+    if (!op_func || ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0))
+        return (_putchar('E') + _putchar('r') + _putchar('r') + _putchar('o') + _putchar('r') + _putchar('\n') + 99);
 
     result = op_func(num1, num2);
 
-    _putchar(result + '0');
+    if (result < 0 || result / 10)
+        _putchar(result / 10 + '0');
+    _putchar(result % 10 + '0');
     _putchar('\n');
 
     return (0);
-}
-
-int _putchar(char c)
-{
-    return c;
 }
