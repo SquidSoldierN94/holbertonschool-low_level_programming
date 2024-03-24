601168e99b8ebd5ff6d149277cde2f0e1eb1a4c6
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-03-24 13:32:24 +0100

    ULTRAKILL - War Without Reason

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
 
 /**
  * print_all - Prints anything
@@ -11,6 +10,7 @@ void print_all(const char * const format, ...)
     va_list args;
     unsigned int i = 0;
     char *str;
+    int num;
 
     va_start(args, format);
 
@@ -19,31 +19,80 @@ void print_all(const char * const format, ...)
         switch (format[i])
         {
             case 'c':
-                printf("%c", va_arg(args, int));
+                putchar(va_arg(args, int));
                 break;
             case 'i':
-                printf("%d", va_arg(args, int));
+                num = va_arg(args, int);
+                if (num < 0)
+                {
+                    putchar('-');
+                    num = -num;
+                }
+                if (num / 10)
+                    print_number(num / 10);
+                putchar(num % 10 + '0');
                 break;
             case 'f':
-                printf("%f", va_arg(args, double));
+                print_float(va_arg(args, double));
                 break;
             case 's':
                 str = va_arg(args, char *);
                 if (str == NULL)
-                    printf("(nil)");
+                    print_string("(nil)");
                 else
-                    printf("%s", str);
+                    print_string(str);
                 break;
         }
 
         if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i' ||
             format[i] == 'f' || format[i] == 's'))
-            printf(", ");
+            putchar(',');
+
+        if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i' ||
+            format[i] == 'f' || format[i] == 's'))
+            putchar(' ');
 
         i++;
     }
 
-    printf("\n");
+    putchar('\n');
 
     va_end(args);
+}
+
+/**
+ * print_number - Prints an integer recursively
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+    if (n / 10)
+        print_number(n / 10);
+    putchar(n % 10 + '0');
+}
+
+/**
+ * print_float - Prints a float
+ * @f: The float to print
+ */
+void print_float(double f)
+{
+    int i_part = (int)f;
+    print_number(i_part);
+    putchar('.');
+    int d_part = (f - i_part) * 1000000;
+    print_number(d_part);
+}
+
+/**
+ * print_string - Prints a string
+ * @s: The string to print
+ */
+void print_string(char *s)
+{
+    while (*s)
+    {
+        putchar(*s);
+        s++;
+    }
 }
