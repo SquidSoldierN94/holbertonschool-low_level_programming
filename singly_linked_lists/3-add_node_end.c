d4e6b71f1ccb2068a6ce6300a50cd8b5e0f8d32a
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-04-03 15:43:16 +0200

    gits

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -14,14 +14,11 @@ list_t *add_node_end(list_t **head, const char *str)
     if (str == NULL)
         return NULL;
 
-    list_t *new_node;
-    char *str_copy;
-
-    new_node = malloc(sizeof(list_t));
+    list_t *new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
         return NULL;
 
-    str_copy = strdup(str);
+    char *str_copy = strdup(str);
     if (str_copy == NULL)
     {
         free(new_node);
